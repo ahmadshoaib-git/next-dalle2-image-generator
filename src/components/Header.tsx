5b139ef520d2ea7630fcbf9309e31549a6fde2d6
Ahmad Shoaib <ahmad.shoaib88@gmail.com>
2023-10-11 11:34:03 +0500

    chore: Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+    return (
+        <div>
+            {/* Left */}
+            <div>
+                <h1>
+                    <span className="text-violet-500">AI</span> Image Generator
+                </h1>
+                <h2 className="text-xs">Powered by DALL.E 2, Chat GPT & Microsoft Azure!</h2>
+            </div>
+            {/* Right */}
+        </div>
+    );
+};
+
+export default Header;
+
